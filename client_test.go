8ebf56f714ac52a87258ef359750355a332d1270
kolach <chikolad@gmail.com>
2019-11-04 11:15:14 -0500

    add backoff test

--- client_test.go
+++ client_test.go
@@ -3,6 +3,7 @@ package apic_test
 import (
 	"io/ioutil"
 	"net/http"
+	"time"
 
 	. "github.com/kolach/apic"
 	. "github.com/onsi/ginkgo"
@@ -12,40 +13,102 @@ import (
 
 var _ = Describe("Client", func() {
 	var server *ghttp.Server
-	var client *Client
 	var NewRequest NewRequestFunc
 
 	BeforeEach(func() {
 		server = ghttp.NewServer()
 		NewRequest = NewRequestFactory(WithBaseURL(server.URL()))
-		client = NewClient()
 	})
 
 	AfterEach(func() {
+		server.Reset()
 		server.Close()
 	})
 
 	Describe("Do", func() {
-		BeforeEach(func() {
-			server.AppendHandlers(
-				ghttp.CombineHandlers(
-					ghttp.VerifyRequest("GET", "/api/orders/1"),
-					ghttp.RespondWith(http.StatusOK, "test"),
-				),
-			)
+		Context("Without backoff", func() {
+			var client *Client
+
+			BeforeEach(func() {
+				client = NewClient()
+				server.AppendHandlers(
+					ghttp.CombineHandlers(
+						ghttp.VerifyRequest("GET", "/api/orders/1"),
+						ghttp.RespondWith(http.StatusOK, "test"),
+					),
+				)
+			})
+
+			It("should make HTTP request", func() {
+				req, _ := NewRequest("GET", "/api/orders/1", nil)
+				res, err := client.Do(req, WithExpectStatus(http.StatusOK))
+				Ω(server.ReceivedRequests()).Should(HaveLen(1))
+				Ω(res.StatusCode).Should(Equal(http.StatusOK))
+				Ω(err).ShouldNot(HaveOccurred())
+
+				// check response body
+				b, errRead := ioutil.ReadAll(res.Body)
+				Ω(errRead).ShouldNot(HaveOccurred())
+				Ω(b).Should(Equal([]byte("test")))
+			})
 		})
 
-		It("should make HTTP request", func() {
-			req, _ := NewRequest("GET", "/api/orders/1", nil)
-			res, err := client.Do(req, WithExpectStatus(http.StatusOK))
-			Ω(server.ReceivedRequests()).Should(HaveLen(1))
-			Ω(res.StatusCode).Should(Equal(http.StatusOK))
-			Ω(err).ShouldNot(HaveOccurred())
+		Context("With backoff", func() {
+			var (
+				client  *Client
+				retries int // to count retries
+			)
 
-			// check response body
-			b, errRead := ioutil.ReadAll(res.Body)
-			Ω(errRead).ShouldNot(HaveOccurred())
-			Ω(b).Should(Equal([]byte("test")))
+			const (
+				maxRetries        = 10
+				successOnAtemptNo = 5
+			)
+
+			BeforeEach(func() {
+				retries = 0 // reset retries counter
+
+				notify := func(err error, dur time.Duration) {
+					retries++ // count retries
+				}
+
+				client = NewClient(
+					WithConstantBackOff(10*time.Millisecond),
+					WithMaxRetries(maxRetries),
+					WithNotify(notify),
+				)
+
+				// responnd with http.StatusForbidden to all /api/orders/101 requests
+				for i := 0; i < maxRetries+1; i++ { // maxRetries + 1 (+1 because there is an original request too)
+					server.AppendHandlers(
+						ghttp.CombineHandlers(
+							ghttp.VerifyRequest("GET", "/api/orders/101"),
+							ghttp.RespondWith(http.StatusForbidden, "test"),
+						),
+					)
+				}
+
+				// but set 6th request to be OK
+				server.SetHandler(successOnAtemptNo,
+					ghttp.CombineHandlers(
+						ghttp.VerifyRequest("GET", "/api/orders/101"),
+						ghttp.RespondWith(http.StatusOK, "test"),
+					),
+				)
+			})
+
+			It("should make HTTP request", func() {
+				req, _ := NewRequest("GET", "/api/orders/101", nil)
+				res, err := client.Do(req, WithExpectStatus(http.StatusOK))
+				Ω(err).ShouldNot(HaveOccurred())
+				Ω(res.StatusCode).Should(Equal(http.StatusOK))
+				Ω(server.ReceivedRequests()).Should(HaveLen(successOnAtemptNo + 1))
+				Ω(retries).Should(Equal(successOnAtemptNo)) // 5 because 6th one is success
+
+				// check response body
+				b, errRead := ioutil.ReadAll(res.Body)
+				Ω(errRead).ShouldNot(HaveOccurred())
+				Ω(b).Should(Equal([]byte("test")))
+			})
 		})
 	})
 })
